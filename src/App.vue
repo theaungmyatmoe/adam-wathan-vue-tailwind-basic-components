4a97edb8f3c227b26c7629d9f645527b59c5fda9
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-09-21 03:52:19 +0630

    feat: add design for tablet

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
-  <main>
-    <div class="px-6 py-12">
-      <img class="h-12" src=".//assets/images/logo.svg" alt="">
-      <img class="mt-6 rounded-xl shadow-xl" src="./assets/images/beach-work.jpg" alt="Woman is working on the beach">
-      <h1 class="mt-6 text-3xl font-bold leading-tight tracking-tight text-gray-800">
+  <main class="bg-gray-100">
+    <div class="px-6 py-12  mx-auto sm:max-w-2xl">
+      <img class="h-12 sm:h-14" src="./assets/images/logo.svg" alt="Workcation Logo">
+      <img class="mt-6 rounded-xl shadow-xl sm:mt-9 sm:h-80 sm:w-full sm:object-cover sm:object-center"
+           src="./assets/images/beach-work.jpg" alt="Woman is working on the beach">
+      <h1 class="mt-6 text-3xl font-bold leading-tight tracking-tight text-gray-800 sm:text-3xl sm:leading-none">
         You can work from anywhere.
         <span class="inline-block mt-2 text-indigo-600">Take advantage of it.</span>
       </h1>
-      <p class="mt-4 text-gray-500 tracking-normal leading-normal">
+      <p class="mt-4 text-gray-500 tracking-normal leading-normal sm:mt-6 sm:text-xl sm:leading-8">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, animi dolorem incidunt ipsum iste labore
         laboriosam perferendis quos sapiente ullam vel, veniam vitae, voluptate. Aliquid distinctio dolore eos odit
         officiis.
       </p>
       <a href="#"
-         class="mt-4 inline-block px-5 py-3 bg-indigo-600  uppercase text-white text-xs font-semibold rounded-lg">Book
+         class="mt-4 inline-block px-5 py-3 bg-indigo-600  uppercase text-white text-xs font-semibold rounded-lg sm:mt-8 sm:py-4 sm:text-base">Book
         your
         escape</a>
     </div>
