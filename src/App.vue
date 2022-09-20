808267683438afcc836bb86c357bc65da10d147e
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-09-21 02:49:05 +0630

    init: initial setup

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <div class="px-6 py-12">
+      <img class="h-12" src=".//assets/images/logo.svg" alt="">
+      <img class="mt-6 rounded-xl shadow-xl" src="./assets/images/beach-work.jpg" alt="Woman is working on the beach">
+      <h1 class="mt-6 text-3xl font-bold leading-tight tracking-tight text-gray-800">
+        You can work from anywhere.
+        <span class="inline-block mt-2 text-indigo-600">Take advantage of it.</span>
+      </h1>
+      <p class="mt-4 text-gray-500 tracking-normal leading-normal">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, animi dolorem incidunt ipsum iste labore
+        laboriosam perferendis quos sapiente ullam vel, veniam vitae, voluptate. Aliquid distinctio dolore eos odit
+        officiis.
+      </p>
+      <a href="#"
+         class="mt-4 inline-block px-5 py-3 bg-indigo-600  uppercase text-white text-xs font-semibold rounded-lg">Book
+        your
+        escape</a>
+    </div>
+  </main>
+</template>
