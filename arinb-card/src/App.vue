9af180697252f31ae7b94b009ec9ba78a5dfec13
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-09-21 10:47:52 +0630

    feat: add composed navbar

--- arinb-card/src/App.vue
+++ arinb-card/src/App.vue
@@ -1,22 +1,12 @@
-<script setup>
-import PropertyCard from "./components/Property.vue";
-
-const property = {
-  image_url: "https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1175&q=80",
-  image_alt: "Rear view of modern house with pool",
-  beds: 3,
-  baths: 2,
-  title: "Modern home in city center ",
-  price_in_cent: "1900000",
-  formatted_price: "$1,900,00",
-  review_count: 34,
-  rating: 4,
-}
-</script>
-
 <template>
-  <main class="antialiased text-gray-900 bg-gray-300 min-h-screen grid place-items-center ">
-    <PropertyCard :property="property"/>
+  <main>
+    <NavBar/>
   </main>
 </template>
+<script>
+import NavBar from "./views/NavBar.vue";
 
+export default {
+  components: {NavBar}
+}
+</script>
---
+++ arinb-card/src/views/NavBar.vue
@@ -0,0 +1,36 @@
+<script setup>
+let isOpen = $ref(false)
+</script>
+
+<template>
+  <header class="bg-gray-900 sm:flex sm:justify-between sm:items-center">
+    <div class="flex justify-between items-center  px-4 py-3">
+      <div>
+        <img class="h-8" src="../assets/logo-inverted.svg" alt="">
+      </div>
+      <div class="sm:hidden">
+        <button
+            class="transition-colors h-8 text-gray-400 outline-none border px-1 py-.5 border-gray-600 rounded hover:text-gray-100 hover:border-gray-400"
+            @click="isOpen = !isOpen"
+        >
+          <svg class="h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+               stroke-width="1.5" stroke="currentColor"
+          >
+            <path v-if="!isOpen" stroke-linecap="round" stroke-linejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+            <path v-if="isOpen" stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
+          </svg>
+        </button>
+      </div>
+    </div>
+    <!-- mobile menu -->
+    <div
+        :class="isOpen ? 'flex': 'hidden'"
+        class="px-2 pt-2 flex pb-4 flex-col sm:flex sm:flex-row"
+    >
+      <a href="#" class="transition-colors text-gray-100 px-2.5 py-2 rounded-lg hover:bg-gray-800">Messages</a>
+      <a href="#" class="transition-colors text-gray-100 px-2.5 py-2 rounded-lg hover:bg-gray-800">Contact Us</a>
+      <a href="#" class="transition-colors text-gray-100 px-2.5 py-2 rounded-lg hover:bg-gray-800">Contact Us</a>
+    </div>
+  </header>
+</template>
